925de277929ae0d73738863f8bf15a13bacaa006
unknown <pnkr06@gmail.com>
2015-07-20 02:30:41 +0530

    port change

--- src/main/java/com/shipinfoapp/Bootstrap.java
+++ src/main/java/com/shipinfoapp/Bootstrap.java
@@ -11,7 +11,7 @@ import static spark.SparkBase.staticFileLocation;
  */
 public class Bootstrap {
     private static final String IP_ADDRESS = System.getenv("OPENSHIFT_DIY_IP") != null ? System.getenv("OPENSHIFT_DIY_IP") : "localhost";
-    private static final int PORT = System.getenv("OPENSHIFT_DIY_IP") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_IP")) : 8082;
+    private static final int PORT = System.getenv("OPENSHIFT_DIY_IP") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_IP")) : 8080;
 
     public static void main(String[] args) throws Exception {
         setIpAddress(IP_ADDRESS);
